2ee953539d1f2548db0bbd6c9875a7dcc1cc0966
Akhil Nazim <anunazim1@gmail.com>
2020-06-25 12:07:35 +0530

    first commit

---
+++ src/components/CardHead.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CardHead() {
+    return (
+        <div className="justify-content-center row">
+        <div >
+            <h1 className="card-head">Nazim</h1>
+        </div>
+        </div>
+    )
+}
+
+export default CardHead
